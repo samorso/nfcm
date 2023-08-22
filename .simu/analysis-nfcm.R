dc7dc91ff307c6c6804d24be32c89e9c22b835a3
samorso <Samuel.Orso@unige.ch>
2023-08-22 08:37:39 +0200

    update

--- .simu/analysis-nfcm.R
+++ .simu/analysis-nfcm.R
@@ -1,6 +1,7 @@
 #---------------------------------
 # Analysis of spline based nonlinear factor copula model
 #---------------------------------
+# different dependence
 mise <- matrix(nr=9,nc=5)
 colnames(mise) <- c("Oracle G", "Initial G", "MLE G", "Oracle H", "MLE H")
 rownames(mise) <- paste0(rep(c("Clayton_", "Gumbel_", "Normal_"), each=3), rep(c(.2,.5,.8),3))
@@ -35,4 +36,32 @@ nll[8,] <- colMeans(res$nll, na.rm=T)
 load(".simu/data/normal_bspline_n_250_d_20_param_0.95.rds")
 mise[9,] <- colMeans(res$mise, na.rm=T)
 nll[9,] <- colMeans(res$nll, na.rm=T)
+mise
+nll
 
+# impact of dimension on estimator of u
+d <- c(2,4,8,16,32,64)
+MC <- 1e3
+rmse <- matrix(nr = MC, ncol = length(d))
+mise <- rep(NA_real_, length(d))
+for(i in seq_along(d)){
+  load(paste0(".simu/data/clayton_bspline_n_250_d_",d[i],"_param_2.rds"))
+  rmse[,i] <- res$rmse[,1]
+  mise[i] <- mean(res$mise[,5], na.rm=T)
+}
+boxplot(rmse)
+mise
+plot(x=d, y=colMeans(rmse,na.rm=T), type="b", pch = 16, xlab = "dimension", ylab = "Mean RMSE of U")
+
+# impact of n on estimation of G
+n <- c(250,500,1000,2000)
+MC <- 1e3
+mise <- nll <- matrix(nr = MC, ncol = length(n))
+for(i in seq_along(n)){
+  load(paste0(".simu/data/gumbel_bspline_n_",n[i],"_d_10_param_2.rds"))
+  mise[,i] <- res$mise[,5]
+  nll[,i] <- res$nll[,3]
+}
+boxplot(nll)
+mise
+plot(x=n, y=colMeans(mise,na.rm=T), type="b", pch = 16, xlab = "sample size", ylab = "MISE")
